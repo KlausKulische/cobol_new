234b77f143308c7d4bde0275acc4c8407585cdb1
Klaus Kulischke <klaus.kulischke@web.de>
2024-02-14 17:54:34 +0100

    Fixed aoc_day_1 part 1
    Signed-off-by: Klaus Kulischke <klaus.kulischke@web.de>

--- aoc_day_1.cob
+++ aoc_day_1.cob
@@ -1,92 +1,149 @@
 000100 identification division.
 000200 program-id. aoc_1.
-000300 
+000300
 000400 environment division.
 000500 input-output section.
 000600  file-control.
 000700  select input_1 assign to 'data.dat'
 000800  organization is line sequential.
-000900 
-001000 data division.
-001100 file section.
-001200 fd input_1.
-001300 01 input_1-file.
-001400  03 in_string  pic x(65).
-001500 
-001600 working-storage section.
-001700 01 ws-eof       pic a.
-001800 01 fil       pic x(4) value space.
-001900 01 num_part_1   pic 9    value zero.
-002000 01 num_part_2   pic 9    value zero.
-002100 01 num_part_3   pic 9    value zero.
-002200 01 num_part_4   pic 9    value zero.
-002300 01 num_part_5   pic 9    value zero.
-002400 01 num_part_6   pic 9    value zero.
-002500 01 num_part_7   pic 9    value zero.
-002600 01 num_part_8   pic 9    value zero.
-002700 01 num_part_9   pic 9    value zero.
-002800 01 num_part_10  pic 9    value zero.
-002900 01 concat_num_1 pic 9(6) value zero.
-003000 01 concat_num_2 pic 9(6) value zero.
-003100 
-003200 procedure division.
-003300    open input input_1.
-003400       perform until ws-eof='Y'
-003500          read input_1
-003600             at end
-003700                move 'Y' to ws-eof
-003800             not at end
-003900               perform num_delimiter
-004000               display "1st " num_part_6 " => 2nd " num_part_7
-004100                 fil "3th " num_part_8 " => 4th " num_part_9
-004200                 fil "5th " num_part_10
-004300               perform num_concat
-004400               add concat_num_1 to concat_num_2
-004500               display "concat num 1 " concat_num_2
-004600               perform num_reset
-004700          end-read
-004800       end-perform
-004900    close input_1.
-005000 
-005100 num_delimiter section.
-005200     unstring in_string delimited by "1" or "2" or "3" or "4"
-005300     or "5" or "6" or "7" or "8" or "9" or "0" into num_part_1
-005400     delimiter in num_part_6
-005500     unstring in_string delimited by "1" or "2" or "3" or "4"
-005600     or "5" or "6" or "7" or "8" or "9" or "0" into num_part_1,
-005700     num_part_2 delimiter in num_part_7.
-005800     unstring in_string delimited by "1" or "2" or "3" or "4"
-005900     or "5" or "6" or "7" or "8" or "9" or "0" into num_part_1,
-006000     num_part_2,num_part_3 delimiter in num_part_8.
-006100     unstring in_string delimited by "1" or "2" or "3" or "4"
-006200     or "5" or "6" or "7" or "8" or "9" or "0" into num_part_1,
-006300     num_part_2,num_part_3,num_part_4 delimiter in num_part_9.
-006400     unstring in_string delimited by "1" or "2" or "3" or "4"
-006500     or "5" or "6" or "7" or "8" or "9" or "0" into num_part_1,
-006600     num_part_2,num_part_3,num_part_4,num_part_5
-006700     delimiter in num_part_10.
-006800 
-006900 num_concat section.
-007000     multiply num_part_6 by 10 giving  concat_num_1
-007100     if num_part_10 not equal 0 then
-007200       add num_part_10 to concat_num_1
-007300     else
-007400      if num_part_9 not equal 0 then
-007500        add num_part_9 to concat_num_1
-007600      else
-007700         if num_part_8 not equal 0 then
-007800           add num_part_8 to concat_num_1
-007900         else
-008000           if num_part_7 not equal 0 then
-008100             add num_part_7 to concat_num_1
-008200           else
-008300             add num_part_6 to concat_num_1
-008400     end-if.
-008500 
-008600 num_reset section.
-008700     move zero to num_part_6, num_part_7, num_part_8, num_part_9,
-008800       num_part_10.
-008900 
-009000*    goback.
-009100 
-009200 end program aoc_1.
+000900  select out_1 assign to 'out.txt'
+001000  organization is line sequential.
+001100
+001200 data division.
+001300 file section.
+001400 fd input_1.
+001500 01 input_1-file.
+001600  03 in_string  pic x(65).
+001700 fd out_1.
+001800 01 out_1_file  pic x(130).
+001900
+002000 working-storage section.
+002100 01 ws-eof       pic a.
+002200 01 ws_pointer_num  pic 99   value zero.
+002300 01 fil          pic x(4) value space.
+002400 01 num_part_1   pic 9    value zero.
+002500 01 num_part_2   pic 9    value zero.
+002600 01 num_part_3   pic 9    value zero.
+002700 01 num_part_4   pic 9    value zero.
+002800 01 num_part_5   pic 9    value zero.
+002900 01 num_part_6   pic 9    value zero.
+003000 01 num_part_7   pic 9    value zero.
+003100 01 num_part_8   pic 9    value zero.
+003200 01 num_part_9   pic 9    value zero.
+003300 01 num_part_10  pic 9    value zero.
+003400 01 num_part_11  pic 9    value zero.
+003500 01 num_part_12  pic 9    value zero.
+003600 01 concat_num_1 pic 9(6) value zero.
+003700 01 concat_num_2 pic 9(6) value zero.
+003800
+003900 01 ws_out_1.
+004000   03 out_string pic x(40).
+004100   03 first_txt  pic x(10) value "    1st : ".
+004200   03 first_num  pic 9.
+004300   03 second_txt pic x(10) value "    2nd : ".
+004400   03 second_num pic 9.
+004500   03 third_txt  pic x(10) value "    3th : ".
+004600   03 third_num  pic 9.
+004700   03 fourth_txt pic x(10) value "    4th : ".
+004800   03 fourth_num pic 9.
+004900   03 fifth_txt  pic x(10) value "    5th : ".
+005000   03 fifth_num  pic 9.
+005100   03 sixth_txt  pic x(10) value "    6th : ".
+005200   03 sixth_num  pic 9.
+005300   03 filler     pic x value space.
+005400   03 concat_out pic x(6).
+005500   03 filler     pic x value space.
+005600   03 concat_o   pic x(6).
+005700   03 filler     pic x value space.
+005800   03 pointer_num pic 99.
+005900
+006000 procedure division.
+006100    open output out_1.
+006200    open input input_1.
+006300       perform until ws-eof='Y'
+006400          read input_1
+006500             at end
+006600                move 'Y' to ws-eof
+006700             not at end
+006800               perform num_delimiter
+006900               perform num_display
+007300               perform num_concat
+007400               add concat_num_1 to concat_num_2
+007600               perform write_out 1 times
+007700               perform num_reset
+007800          end-read
+007900       end-perform
+008000    close input_1, out_1.
+008100
+008200 write_out section.
+008300     move in_string     to  out_string.
+008400     move num_part_6    to  first_num.
+008500     move num_part_7    to  second_num.
+008600     move num_part_8    to  third_num.
+008700     move num_part_9    to  fourth_num.
+008800     move num_part_10   to  fifth_num.
+008900     move num_part_11   to  sixth_num.
+009000     move concat_num_2  to  concat_out.
+009100     move concat_num_1  to  concat_o.
+009200     move ws_pointer_num to pointer_num.
+009300     write out_1_file   from  ws_out_1 after advancing 1 line.
+009400
+009500 num_pointer section.
+009600     unstring in_string delimited by all "1" or "2" or "3" or "4"
+009700     or "5" or "6" or "7" or "8" or "9" or "0" into num_part_1
+009800     count ws_pointer_num.
+009900
+010000 num_display section.
+010100     perform num_pointer.
+010200
+010300 num_delimiter section.
+010400     unstring in_string delimited by "1" or "2" or "3" or "4"
+010500     or "5" or "6" or "7" or "8" or "9" or "0" into num_part_1
+010600     delimiter in num_part_6.
+010700     unstring in_string delimited by "1" or "2" or "3" or "4"
+010800     or "5" or "6" or "7" or "8" or "9" or "0" into num_part_1,
+010900     num_part_2 delimiter in num_part_7.
+011000     unstring in_string delimited by "1" or "2" or "3" or "4"
+011100     or "5" or "6" or "7" or "8" or "9" or "0" into num_part_1,
+011200     num_part_2,num_part_3 delimiter in num_part_8.
+011300     unstring in_string delimited by "1" or "2" or "3" or "4"
+011400     or "5" or "6" or "7" or "8" or "9" or "0" into num_part_1,
+011500     num_part_2,num_part_3,num_part_4 delimiter in num_part_9.
+011600     unstring in_string delimited by "1" or "2" or "3" or "4"
+011700     or "5" or "6" or "7" or "8" or "9" or "0" into num_part_1,
+011800     num_part_2,num_part_3,num_part_4,num_part_5
+011900     delimiter in num_part_10.
+012000     unstring in_string delimited by "1" or "2" or "3" or "4"
+012100     or "5" or "6" or "7" or "8" or "9" or "0" into num_part_1,
+012200     num_part_2,num_part_3,num_part_4,num_part_5,num_part_12
+012300     delimiter in num_part_11.
+012400
+012500 num_concat section.
+012600     multiply num_part_6 by 10 giving  concat_num_1
+012700     if num_part_11 not equal 0 then
+012800       add num_part_11 to concat_num_1
+012900     else
+013000       if num_part_10 not equal 0 then
+013100         add num_part_10 to concat_num_1
+013200       else
+013300        if num_part_9 not equal 0 then
+013400          add num_part_9 to concat_num_1
+013500        else
+013600           if num_part_8 not equal 0 then
+013700             add num_part_8 to concat_num_1
+013800           else
+013900             if num_part_7 not equal 0 then
+014000               add num_part_7 to concat_num_1
+014100             else
+014200               add num_part_6 to concat_num_1
+014300             end-if
+014400           end-if
+014500        end-if
+014600       end-if
+014700     end-if.
+014800
+014900 num_reset section.
+015000     move zero to num_part_6, num_part_7, num_part_8, num_part_9,
+015100       num_part_10, num_part_11.
+015200
+015300 end program aoc_1.
